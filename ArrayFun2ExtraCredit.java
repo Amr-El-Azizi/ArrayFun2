787f1e0a1620fccaddbb27f968f8a8b86e8aa560
Amr-El-Azizi <ammourazz@yahoo.com>
2014-11-06 14:45:55 -0500

    I did it

--- ArrayFun2ExtraCredit.java
+++ ArrayFun2ExtraCredit.java
@@ -2,10 +2,10 @@ public class ArrayFun2ExtraCredit
 {
     public ArrayFun2ExtraCredit()
     {
-              int[] a =  {2,1,3,2,1};
-        int[] b =  {1,3,2,3,2,2,1,3};
+        int[] a =  {4,5,6,7};
+        int[] b =  {2,3,4,5};
         
-        print(product(a,b,10));
+        print(productany(a,b,10));
     }
     
     int[] invert(int[] nums)
@@ -90,7 +90,7 @@ public class ArrayFun2ExtraCredit
         return output;
     }
     
-    int[] product5(int[] num1, int[] num2, int base)
+    int[] product5indiv(int[] num1, int[] num2, int base)
     {
         int[] output = new int[5];
         int product = 0;
@@ -123,6 +123,58 @@ public class ArrayFun2ExtraCredit
         throw new ArithmeticException("Multiplication Overflow Error");
         return output;
     }
+    
+    int[] productany(int[] num1, int[] num2, int base)
+    {
+        int num1l = num1.length;
+        int num2l = num2.length;
+        int[] output = new int[num1l+num2l];
+        int product = 0;
+        int carry = 0;
+        int[] temp = new int[num1l+num2l];
+        int tens = 0;
+        for(int x = num1l-1; x >= 0; x--)
+        {
+            temp = resetArray(temp, temp.length-tens+1);
+            for(int i = num2l-1; i >= 0; i--)
+            {
+                product = num1[x] * num2[i];
+                if(product + carry >= base)
+                {
+                    if((product + carry) % base == 0)
+                    {
+                        temp[i+num1l] = 0;
+                        carry = (product + carry)/base;
+                    }
+                    else
+                    {
+                        temp[i+num1l] = product%base + carry;
+                        carry = product/ base;
+                    }
+                }
+                else
+                {
+                    temp[i+num1l] = product + carry;
+                    carry = 0;
+                }
+            }
+            temp[num1l-1] += carry;
+            print(temp);
+            System.out.println("");
+            for(int i = 0; i < tens; i++)
+            {
+                temp = shiftleft(temp);
+            }
+            print(temp);
+            System.out.println("");
+            output = add(output, temp, base);
+            tens ++;
+            carry = 0;
+        }
+        return shiftright(output);
+    }
+    
+
     
         int[] add(int[] num1, int[] num2, int base)
     {
@@ -175,61 +227,35 @@ public class ArrayFun2ExtraCredit
         
         if(carry > 0)
         throw new ArithmeticException("Addition Overflow Error");
-        return output;
+        return (output);
     }
     
-    int[] product(int[] num1, int[] num2, int base)
-    {
-        int nl1 = num1.length;
-        int nl2 = num2.length;
-        if(nl1 > 100 || nl2 > 100)
-        throw new ArithmeticException("Out of Bounds Error");
-        
-        int[] snum = num1;
-        int[] lnum = num2;
-        int lnuml = nl2;
-        int snuml = nl1;
-        
-        if(nl1 > nl2)
-        {
-            snum = num2;
-            lnum = num1;
-            lnuml = nl1;
-            snuml = nl2;
-        }
-        int ldifference = lnuml-snuml;
-        int[] output = new int[lnuml];
-        int product = 0;
-        int carry = 0;
-        
-        for(int i = snuml-1; i >= 0; i--)
-        {
-            product = lnum[i+ldifference] * snum[i];
-            if(product + carry >= base)
-            {
-                if((product + carry) % base == 0)
-                {
-                    output[i] = 0;
-                    carry = (product + carry)/base;
-                    
-                }
-                else
-                {
-                    output[i] = product%base + carry;
-                    carry = product/ base;
-                }
-            }
-            else
-            {
-                output[i] = product + carry;
-                carry = 0;
-            }
-        }
-        
-        if(carry > 0)
-        throw new ArithmeticException("Multiplication Overflow Error");
-        return output;
-    }
+   
+   // METHODS FOR PRODUCT
+       
+   public int[] shiftleft(int[] nums)
+   {
+        int nl = nums.length;
+        int[] temp = new int[nl + 1];
+        for(int i = 0; i < nl; i ++ )
+        temp[i] = nums[i];
+        return temp;
+   }
+   
+      public int[] shiftright(int[] nums)
+   {
+        int nl = nums.length;
+        int[] temp = new int[nl - 1];
+        for(int i = nl-2; i >= 0; i -- )
+        temp[i] = nums[i];
+        return temp;
+   }
+   
+   public int[] resetArray(int[] nums, int len)
+   {
+       int[] blank = new int[len];
+       return blank;
+   }
         /// HELPERS
 public void print(String s){
     System.out.println("printing : " + s ) ;
@@ -244,7 +270,7 @@ for( boolean inB : b)
 
 public void print(int[] nums){
 for( int n : nums)
-    System.out.println(n);
+    System.out.print(n);
 
 }
 
